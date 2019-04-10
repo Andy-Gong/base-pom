57a58093bee4df8dd0be9539d70de45611958a18
Andy-Gong <gongxch@163.com>
2019-04-10 23:46:06 +0800

    add code-style for eclipse and intellij

--- src/main/java/test/checkstyle/CheckstyleTest.java
+++ src/main/java/test/checkstyle/CheckstyleTest.java
@@ -4,6 +4,7 @@ public class CheckstyleTest {
 
     private static final String abc = "123";
 
-      public void test() {
+    public void test() {
+        System.out.println("fdhjakfhdjafhdjkafhjdlakfhdjlafhjdklafhdjaklfhdjakfhdjaklfhjdalfdsahfjdkahfjdkalhfjdlkafhjdalfhdljaffjakdfhlajkdfhajdklf");
     }
 }
